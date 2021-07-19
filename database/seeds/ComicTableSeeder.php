26bea0c5b48f6a35772b3bb2baeadebcef8b19d7
BaccanoB <beatrice.baccano@yahoo.it>
2021-07-19 15:37:58 +0200

    creato database e popolato con seeder

---
+++ database/seeds/ComicTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $arrayComic=config('comic');
+
+        foreach ($arrayComic as $item) {
+            $comic = new Comic();
+
+            $comic->title=$item['title'];
+            $comic->description=$item['description'];
+            $comic->thumb=$item['thumb'];
+            $comic->price=$item['price'];
+            $comic->series=$item['series'];
+            $comic->sale_date=$item['sale_date'];
+            $comic->type=$item['type'];
+
+            $comic->save();
+        }
+
+    }
+}
