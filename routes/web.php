831c6e446fd518ad70e0c01b1b38b4445259e5cc
BaccanoB <beatrice.baccano@yahoo.it>
2021-07-19 16:18:53 +0200

    cratato resource controller,inserita nuova rotta ed inseriti link per libreria bootstrap

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'ComicController@index');
+Route::get('/', 'HomeController@index');
+
+Route::resource('comics','ComicController');
+
